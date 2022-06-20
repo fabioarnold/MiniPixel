3a3ea8547cbfaf6f6f6fbd94a1c28848b7a756ac
Fabio Arnold <fabio@fabioarnold.de>
2022-06-20 19:36:20 +0200

    SDL2 include prefix for Windows

--- src/c.zig
+++ src/c.zig
@@ -1,5 +1,9 @@
+const builtin = @import("builtin");
+
+const sdl2_include_prefix = if (builtin.os.tag == .windows) "SDL2/" else "";
+
 pub usingnamespace @cImport({
-    @cInclude("SDL.h");
-    @cInclude("SDL_opengl.h");
-    @cInclude("SDL_syswm.h");
+    @cInclude(sdl2_include_prefix ++ "SDL.h");
+    @cInclude(sdl2_include_prefix ++ "SDL_opengl.h");
+    @cInclude(sdl2_include_prefix ++ "SDL_syswm.h");
 });
