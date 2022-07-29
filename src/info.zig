28cc7c9d2ce388409e44932902009ddbe708e4b5
Fabio Arnold <fabio@fabioarnold.de>
2022-07-29 18:49:48 +0200

    Bump version to 0.2.1

--- src/info.zig
+++ src/info.zig
@@ -1,5 +1,5 @@
 pub const org_name = "FabioWare";
 pub const app_name = "Mini Pixel";
-pub const version = "0.2.0";
+pub const version = "0.2.1";
 pub const link_itchio = "https://fabioarnold.itch.io/mini-pixel";
 pub const link_github = "https://github.com/fabioarnold/minipixel";
