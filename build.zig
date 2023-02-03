fae840413de3c91607b58e7b5c0c7f070411d741
Fabio Arnold <fabio@fabioarnold.de>
2023-02-03 19:56:46 +0100

    Update to new build API

--- build.zig
+++ build.zig
@@ -1,34 +1,35 @@
 const std = @import("std");
 const fs = std.fs;
 const mem = std.mem;
-const Builder = std.build.Builder;
-const FileSource = std.build.FileSource;
 const Pkg = std.build.Pkg;
 
 const nanovg_build = @import("deps/nanovg-zig/build.zig");
 
-const win32 = Pkg{ .name = "win32", .source = FileSource.relative("deps/zigwin32/win32.zig") };
-const nfd = Pkg{ .name = "nfd", .source = FileSource.relative("deps/nfd-zig/src/lib.zig") };
-const nanovg = Pkg{ .name = "nanovg", .source = FileSource.relative("deps/nanovg-zig/src/nanovg.zig") };
-const s2s = Pkg{ .name = "s2s", .source = FileSource.relative("deps/s2s/s2s.zig") };
-const gui = Pkg{ .name = "gui", .source = FileSource.relative("src/gui/gui.zig"), .dependencies = &.{nanovg} };
+const win32 = Pkg{ .name = "win32", .source = .{ .path = "deps/zigwin32/win32.zig" } };
+const nfd = Pkg{ .name = "nfd", .source = .{ .path = "deps/nfd-zig/src/lib.zig" } };
+const nanovg = Pkg{ .name = "nanovg", .source = .{ .path = "deps/nanovg-zig/src/nanovg.zig" } };
+const s2s = Pkg{ .name = "s2s", .source = .{ .path = "deps/s2s/s2s.zig" } };
+const gui = Pkg{ .name = "gui", .source = .{ .path = "src/gui/gui.zig" }, .dependencies = &.{nanovg} };
 
-fn installPalFiles(b: *Builder) void {
+fn installPalFiles(b: *std.Build) void {
     const pals = [_][]const u8{ "arne16.pal", "arne32.pal", "db32.pal", "default.pal", "famicube.pal", "pico-8.pal" };
     inline for (pals) |pal| {
         b.installBinFile("data/palettes/" ++ pal, "palettes/" ++ pal);
     }
 }
 
-pub fn build(b: *Builder) !void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
     const automated_testing = b.option(bool, "automated-testing", "Enable automated testing") orelse false;
 
-    const exe = b.addExecutable("minipixel", "src/main.zig");
+    const exe = b.addExecutable(.{
+        .name = "minipixel",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     exe.setMainPkgPath(".");
-    exe.setBuildMode(mode);
-    exe.setTarget(target);
 
     const exe_options = b.addOptions();
     exe.addOptions("build_options", exe_options);
@@ -54,7 +55,7 @@ pub fn build(b: *Builder) !void {
     } else if (exe.target.isDarwin()) {
         exe.addCSourceFile("src/c/sdl_hacks.m", &.{});
     }
-    const c_flags: []const []const u8 = if (mode == .Debug)
+    const c_flags: []const []const u8 = if (optimize == .Debug)
         &.{ "-std=c99", "-D_CRT_SECURE_NO_WARNINGS", "-O0", "-g" }
     else
         &.{ "-std=c99", "-D_CRT_SECURE_NO_WARNINGS" };
@@ -65,7 +66,7 @@ pub fn build(b: *Builder) !void {
     nanovg_build.addNanoVGPackage(exe);
     exe.addPackage(s2s);
     exe.addPackage(gui);
-    const nfd_lib = @import("deps/nfd-zig/build.zig").makeLib(b, mode, target);
+    const nfd_lib = @import("deps/nfd-zig/build.zig").makeLib(b, target, optimize);
     exe.linkLibrary(nfd_lib);
     exe.linkSystemLibrary("SDL2");
     if (exe.target.isWindows()) {
@@ -86,13 +87,14 @@ pub fn build(b: *Builder) !void {
         exe.linkSystemLibrary("X11");
     }
     exe.linkLibC();
-    if (b.is_release) exe.strip = true;
     exe.install();
 
     installPalFiles(b);
 
-    const test_cmd = b.addTest("src/tests.zig");
-    test_cmd.setBuildMode(mode);
+    const test_cmd = b.addTest(.{
+        .root_source_file = .{ .path = "src/tests.zig" },
+        .optimize = optimize,
+    });
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&test_cmd.step);
 
